8e5d3b2af42c31244d959c3ff5ca2f7136e41154
Lucas Wang <lucas@dgraph.io>
2018-10-30 16:25:53 -0700

    Adding the test.sh to automatically setup and teardown the external cluster

--- setup-cluster.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-if [ -z $GOPATH ]; then
-    echo "Error: the GOPATH environment variable is not set"; exit 1
-else
-    cd $GOPATH/src/github.com/dgraph-io/dgraph/dgraph; ./run.sh
-fi
