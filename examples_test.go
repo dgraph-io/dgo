4adc2fe5a5e9dbb4dba19380c34c7c41940e59af
Martin Martinez Rivera <martinmr@dgraph.io>
2019-04-08 17:41:52 -0700

    Fix tests by login with groot id and password.

--- examples_test.go
+++ examples_test.go
@@ -21,6 +21,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/dgraph-io/dgo"
@@ -37,7 +38,22 @@ func getDgraphClient() (*dgo.Dgraph, CancelFunc) {
 	}
 
 	dc := api.NewDgraphClient(conn)
-	return dgo.NewDgraphClient(dc), func() {
+	dg := dgo.NewDgraphClient(dc)
+	ctx := context.Background()
+
+	for {
+        // keep retrying until we succeed or receive a non-retriable error
+        err = dg.Login(ctx, "groot", "password")
+        if err == nil || !strings.Contains(err.Error(), "Please retry") {
+            break
+        }
+        time.Sleep(time.Second)
+    }
+	if err != nil {
+		log.Fatalf("While trying to login %v", err.Error())
+	}
+
+	return dg, func() {
 		if err := conn.Close(); err != nil {
 			log.Printf("Error while closing connection:%v", err)
 		}
