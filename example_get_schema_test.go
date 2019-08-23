9679d10ccd81ee4789abba06cfa2ab99a0d66277
Aman Mangal <aman@dgraph.io>
2019-08-23 08:00:01 +0530

    Update import paths to use v2

--- example_get_schema_test.go
+++ example_get_schema_test.go
@@ -21,8 +21,8 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/dgraph-io/dgo"
-	"github.com/dgraph-io/dgo/protos/api"
+	"github.com/dgraph-io/dgo/v2"
+	"github.com/dgraph-io/dgo/v2/protos/api"
 	"google.golang.org/grpc"
 )
 
