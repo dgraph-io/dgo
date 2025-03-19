bce81d1017c34fb4df5dd03dfd46a657028a5fba
Matthew McNeely <matthew.mcneely@gmail.com>
2025-03-19 19:42:40 -0400

    Update tests; add self-cert test

--- login_test.go
+++ login_test.go
@@ -9,7 +9,8 @@ import (
 )
 
 func Test_connectLocal(t *testing.T) {
-	dg, err := dgo.Connect("dgraph://127.0.0.1:9080")
+	t.Skip("skipping local connection")
+	dg, err := dgo.Open("dgraph://127.0.0.1:9080")
 	if err != nil {
 		t.Fatalf("Failed to connect to local Dgraph: %v", err)
 	}
@@ -22,8 +23,30 @@ func Test_connectLocal(t *testing.T) {
 	fmt.Println(string(resp.Json))
 }
 
+func Test_connectLocalTLS(t *testing.T) {
+	t.Skip("skipping local TLS connection")
+	dg, err := dgo.Open("dgraph://127.0.0.1:9080?sslmode=require")
+	if err != nil {
+		t.Fatalf("Failed to connect to local Dgraph: %v", err)
+	}
+	defer dg.Close()
+	// Query to schema to check the connection
+	query := `
+	{
+		me(func: uid(1)) {
+			uid
+		}
+	}`
+	resp, err := dg.NewReadOnlyTxn().BestEffort().Query(context.Background(), query)
+	if err != nil {
+		t.Fatalf("Failed to query schema: %v", err)
+	}
+	fmt.Println(string(resp.Json))
+}
+
 func Test_connectLocalACL(t *testing.T) {
-	dg, err := dgo.Connect("dgraph://groot:password@127.0.0.1:9080")
+	t.Skip("skipping local ACL connection")
+	dg, err := dgo.Open("dgraph://groot:password@127.0.0.1:9080")
 	if err != nil {
 		t.Fatalf("Failed to connect to local Dgraph: %v", err)
 	}
@@ -37,7 +60,8 @@ func Test_connectLocalACL(t *testing.T) {
 }
 
 func Test_connectLocalACLAsUser(t *testing.T) {
-	dg, err := dgo.Connect("dgraph://alice:supersecret@127.0.0.1:9080")
+	t.Skip("skipping local ACL connection as user")
+	dg, err := dgo.Open("dgraph://alice:supersecret@127.0.0.1:9080")
 	if err != nil {
 		t.Fatalf("Failed to connect to local Dgraph: %v", err)
 	}
@@ -51,12 +75,13 @@ func Test_connectLocalACLAsUser(t *testing.T) {
 }
 
 func Test_connectCloud(t *testing.T) {
+	t.Skip("skipping cloud connection")
 	// https://throbbing-field-510005.us-west-2.aws.cloud.dgraph.io/graphql
 	// throbbing-field-510005.grpc.us-west-2.aws.cloud.dgraph.io:443
 	host := "throbbing-field-510005.grpc.us-west-2.aws.cloud.dgraph.io:443"
 	apiKey := "NWEzM2VkNmI3MTM3YjIxN2ExYjQxYmJkODFlYWJhMWI="
-	connStr := fmt.Sprintf("dgraph://%s?api-key=%s", host, apiKey)
-	dg, err := dgo.Connect(connStr)
+	connStr := fmt.Sprintf("dgraph://%s?apikey=%s", host, apiKey)
+	dg, err := dgo.Open(connStr)
 	if err != nil {
 		t.Fatalf("Failed to connect to local Dgraph: %v", err)
 	}
